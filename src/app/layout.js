db2f36a2336066746299dff92c41612b1bb8f73b
Karthik <karthik.nishanth06@gmail.com>
2025-03-19 22:27:35 +0530

    add

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono, Zilla_Slab } from "next/font/google";
 import "./globals.css";
+import "@/styles/safari-optimize.css";
 import Navbar from "@/components/Navbar";
 import { OptimizedMotion } from "@/lib/framerConfig";
 
